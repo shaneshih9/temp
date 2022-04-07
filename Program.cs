2552c4fa36094a513c58b30d31911c879b76c4b3
shane <shaneshih9@gmail.com>
2022-04-07 11:03:17 +0530

    added

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace sample1
+{
+    class input
+        
+    {
+        static void Main(string[] args)
+        {
+            student shane = new student("shane",100);
+       
+            //shane.getDetail(834,"shane shih",99,98,93);
+            shane.average();
+            shane.display();
+            student shih = new student("shih", 99);
+            
+            //shane.getDetail(834,"shane shih",99,98,93);
+            shane.average();
+            shane.display();
+            
+        }
+        
+    }
+}
